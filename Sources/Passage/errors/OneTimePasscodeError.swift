10b22afb55f5cee409e9c427b8a4bcbfe03cba20
Ricky Padilla <rickycpadilla@gmail.com>
2024-08-07 07:21:33 -0600

    Added Magic Link, One-Time Passcode, and Social functionality (#6)
    * feat(PSG-4336): added Passage App w appInfo and userExists methods
    
    * feat(PSG-4336): added `createUser` method
    
    * feat(PSG-4338): added PassageMagicLink class and error
    
    * feat(PSG-4339): added otp class
    
    * feat(PSG-4340): added social auth

---
+++ Sources/Passage/errors/OneTimePasscodeError.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+public enum OneTimePasscodeError: PassageError {
+    
+    case exceededAttempts(message: String)
+    case invalidIdentifier(message: String)
+    case unspecified(message: String)
+    case userAlreadyExists(message: String)
+    case userDoesNotExist(message: String)
+    
+    public var errorDescription: String {
+        switch self {
+        case
+            .exceededAttempts(message: let message),
+            .invalidIdentifier(message: let message),
+            .unspecified(let message),
+            .userAlreadyExists(let message),
+            .userDoesNotExist(let message):
+            return message
+        }
+    }
+    
+    public static func convert(error: Error) -> OneTimePasscodeError {
+        // Check if error is already proper
+        if let error = error as? OneTimePasscodeError {
+            return error
+        }
+        // Handle client error
+        if let errorResponse = error as? ErrorResponse,
+           let (_, errorData) = PassageErrorData.getData(from: errorResponse)
+        {
+            switch errorData.code {
+            case Model400Code.request.rawValue:
+                if errorData.error == "user: already exists." {
+                    return .userAlreadyExists(message: errorData.error)
+                } else {
+                    return .invalidIdentifier(message: errorData.error)
+                }
+            case Model404Code.userNotFound.rawValue:
+                return .userDoesNotExist(message: errorData.error)
+            case "exceeded_attempts":
+                return .exceededAttempts(message: errorData.error)
+            default: ()
+            }
+        }
+        return .unspecified(message: "unspecified error")
+    }
+    
+}
---
+++ Sources/Passage/interfaces/PassageOneTimePasscode.swift
@@ -0,0 +1,80 @@
+import Foundation
+
+/// A class allowing interaction with the Passage One-Time Passcode API.
+public class PassageOneTimePasscode {
+    
+    private let appId: String
+    
+    init(appId: String) {
+        self.appId = appId
+    }
+    
+    /// Creates and sends a one-time passcode to register the user. The user will receive an email or text with the code.
+    ///
+    /// - Parameters:
+    ///   - identifier: user's email or phone number
+    ///   - language: optional language string for localizing emails, if no lanuage or an invalid language is provided the application default lanuage will be used
+    /// - Returns: `OneTimePasscodeResponse` includes the one-time passcode ID, which will be used to activate the one-time passcode
+    /// - Throws: `OneTimePasscodeError`
+    public func register(identifier: String, language: String? = nil) async throws -> OneTimePasscodeResponse {
+        do {
+           let request = RegisterOneTimePasscodeRequest(
+               identifier: identifier,
+               language: language
+           )
+           return try await RegisterAPI
+               .registerOneTimePasscode(
+                   appId: appId,
+                   registerOneTimePasscodeRequest: request
+               )
+       } catch {
+           throw OneTimePasscodeError.convert(error: error)
+       }
+    }
+    
+    /// Creates and send a one-time passcode to login the user. The user will receive an email or text with the code.
+    ///
+    /// - Parameters:
+    ///   - identifier: user's email or phone number
+    ///   - language: optional language string for localizing emails, if no lanuage or an invalid language is provided the application default lanuage will be used
+    /// - Returns: `OneTimePasscodeResponse` includes the one-time passcode ID, which will be used to activate the one-time passcode
+    /// - Throws: `OneTimePasscodeError`
+    public func login(identifier: String, language: String? = nil) async throws -> OneTimePasscodeResponse {
+        do {
+            let request = LoginOneTimePasscodeRequest(
+                identifier: identifier,
+                language: language
+            )
+            return try await LoginAPI
+                .loginOneTimePasscode(
+                    appId: appId,
+                    loginOneTimePasscodeRequest: request
+                )
+        } catch {
+            throw OneTimePasscodeError.convert(error: error)
+        }
+    }
+    
+    /// Completes a one-time passcode flow by activating the one-time passcode.
+    ///
+    /// - Parameter otp: string - The one-time provided by your user
+    /// - Parameter id: string - The one-time passcode id returned from login or register method
+    /// - Returns: `AuthResult` The AuthResult struct contains the user's authentication token.
+    /// - Throws: `OneTimePasscodeError`
+    public func activate(otp: String, id: String) async throws -> AuthResult {
+        do {
+            let request = ActivateOneTimePasscodeRequest(
+                otp: otp,
+                otpId: id
+            )
+            let response = try await OTPAPI.activateOneTimePasscode(
+                appId: appId,
+                activateOneTimePasscodeRequest: request
+            )
+            return response.authResult
+        } catch {
+            throw OneTimePasscodeError.convert(error: error)
+        }
+    }
+
+}
