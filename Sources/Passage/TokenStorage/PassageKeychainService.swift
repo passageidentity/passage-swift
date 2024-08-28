d9001b654e0c5684fdb4a1f0e8005506af2f51aa
Ricky Padilla <rickycpadilla@gmail.com>
2024-08-28 16:06:03 -0600

    feat(PSG-4343): added keychain service

---
+++ Sources/Passage/TokenStorage/PassageKeychainService.swift
@@ -0,0 +1,59 @@
+import Foundation
+import Security
+
+internal class PassageKeychainService {
+    
+    private let service = Bundle.main.bundleIdentifier ?? "PassageKeychainService"
+    
+    internal func addString(key: String, value: String) {
+        let data = Data(value.utf8)
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+        // Remove any existing item before adding
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+    
+    internal func getString(key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+        if status == errSecSuccess, let data = dataTypeRef as? Data {
+            return String(data: data, encoding: .utf8)
+        }
+        return nil
+    }
+    
+    internal func updateString(key: String, value: String) {
+        let data = Data(value.utf8)
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key
+        ]
+        let attributes: [String: Any] = [
+            kSecValueData as String: data
+        ]
+        SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+    }
+    
+    internal func deleteString(key: String) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: key
+        ]
+        SecItemDelete(query as CFDictionary)
+    }
+    
+}
